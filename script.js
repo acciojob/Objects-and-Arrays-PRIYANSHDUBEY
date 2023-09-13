4fbfff5039a4b3fd916688a90a58e4a42f7e7db1
PRIYANSHDUBEY <87897527+PRIYANSHDUBEY@users.noreply.github.com>
2023-09-13 16:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a reference to players
+const team = players;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a shallow copy of players using the spread operator
+const team1 = [...players];
+
+// Create a shallow copy of the person object using Object.assign
+const cap1 = Object.assign({}, person);
+
+// You can also use the object destructuring to create a shallow copy of the person object
+// const cap1 = { ...person };
+
+// Log the variables to verify the results
+console.log(team);   // This will refer to the original players array
+console.log(team1);  // This will be a copy of the players array
+console.log(cap1);   // This will be a copy of the person object
