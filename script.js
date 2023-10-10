352e61f935391e72ce824f36268843116182adfa
PRIYANSHDUBEY <87897527+PRIYANSHDUBEY@users.noreply.github.com>
2023-10-11 01:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,19 +4,8 @@ const person = {
   age: 80,
 };
 
-// Create a reference to players
 const team = players;
 
-// Create a shallow copy of players using the spread operator
 const team1 = [...players];
 
-// Create a shallow copy of the person object using Object.assign
-const cap1 = Object.assign({}, person);
-
-// You can also use the object destructuring to create a shallow copy of the person object
-// const cap1 = { ...person };
-
-// Log the variables to verify the results
-console.log(team);   // This will refer to the original players array
-console.log(team1);  // This will be a copy of the players array
-console.log(cap1);   // This will be a copy of the person object
+const cap1 = players.[...person];
