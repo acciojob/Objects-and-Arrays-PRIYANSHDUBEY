28cffd8076bdad4472f1f771f6040ef5491f5a7d
PRIYANSHDUBEY <87897527+PRIYANSHDUBEY@users.noreply.github.com>
2023-10-11 01:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const person = {
   age: 80,
 };
 
+// Create a reference to the array players
 const team = players;
 
+// Create a copy of the array players using the spread operator
 const team1 = [...players];
 
-const cap1 = players.[...person];
+// Create a copy of the object person using the spread operator
+const cap1 = { ...person };
